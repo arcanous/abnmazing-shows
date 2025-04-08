949a5d02976acc1af2a4d8b905027b6c1a76054b
Harijs Deksnis <harijs.deksnis@gmail.com>
2025-04-08 22:12:49 +0200

    feat: improve ShowCard to show name and rating

--- components/ShowCard.vue
+++ components/ShowCard.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink 
     :to="`/show/${show.id}`" 
-    class="flex-none w-40 transform transition duration-200 hover:scale-105"
+    class="flex-none w-40 transform transition duration-200 hover:scale-105 relative group"
   >
     <NuxtImg 
       :src="show.image.medium" 
@@ -11,6 +11,13 @@
       class="rounded-md shadow-lg"
       loading="lazy"
     />
+    <div class="absolute bottom-0 left-0 right-0 p-3 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-b-md">
+      <p class="text-white text-sm font-medium truncate">{{ show.name }}</p>
+      <div class="flex items-center mt-1">
+        <span v-if="show.rating?.average" class="text-yellow-400 text-xs">★ {{ show.rating.average }}</span>
+        <span v-else class="text-gray-400 text-xs">No rating</span>
+      </div>
+    </div>
   </NuxtLink>
 </template>
 
