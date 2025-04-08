ddcd1f0d55ff15c6381be0c1a6cae909a051b5e5
Harijs Deksnis <harijs.deksnis@gmail.com>
2025-04-08 14:50:39 +0200

    chore: set up vitest with a basic test

---
+++ vitest.setup.ts
@@ -0,0 +1,14 @@
+import { afterAll, afterEach, beforeAll } from 'vitest'
+
+// Minimal setup file, add more global setup as needed
+beforeAll(() => {
+  // any setup before all tests
+})
+
+afterAll(() => {
+  // any cleanup after all tests
+})
+
+afterEach(() => {
+  // cleanup after each test
+}) 
