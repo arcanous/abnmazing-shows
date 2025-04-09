9d80e7731ee786caf9bbdbdb37181e475dae9781
Harijs Deksnis <harijs.deksnis@gmail.com>
2025-04-09 16:42:15 +0200

    style: use transition between layouts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
   ],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'page', mode: 'out-in' },
     head: {
       title: 'ABNmazing Shows',
       htmlAttrs: {
